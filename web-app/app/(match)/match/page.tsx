0c258dd10c1861a4b1e9277497013332af4dc275
JunhoHwoang <154561586+JunhoHwoang@users.noreply.github.com>
2026-02-12 02:01:01 -0500

    created mock data for profile list and like list

--- web-app/app/(match)/match/page.tsx
+++ web-app/app/(match)/match/page.tsx
@@ -1,7 +1,9 @@
 import { requireAuth } from "@/lib/auth";
+import profiles from "@/mock/profiles.json";
 
 export default async function Match() {
   const user = await requireAuth();
+  console.log(profiles);
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
