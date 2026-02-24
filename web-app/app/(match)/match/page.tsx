792911b006dfa7eb6a3fff17ae1061bf4768fab8
JunhoHwoang <154561586+JunhoHwoang@users.noreply.github.com>
2026-02-23 21:03:14 -0500

    Moved the function to _action.ts

--- web-app/app/(match)/match/page.tsx
+++ web-app/app/(match)/match/page.tsx
@@ -2,35 +2,9 @@ import { requireAuth } from "@/lib/auth";
 import { MatchClient } from "./match-client";
 import profiles from "@/mock/profiles.json";
 import { UserProfile } from "./discovery-page";
-import { createClient } from "@/lib/supabase/server";
 
 export default async function MatchPage() {
   const user = await requireAuth();
-  const supabase = await createClient();
-
-  const { data: likedSwipes, error: likedSwipesError } = await supabase
-    .from("discovery_swipes")
-    .select("user_id, created_at, message")
-    .eq("target_user_id", user.id)
-    .eq("action", "like")
-    .order("created_at", { ascending: false });
-
-  // Create a custom error page (error.tsx) under /match if this fails
-  if (likedSwipesError) {
-    throw new Error(
-      `Failed to fetch liked profiles: ${likedSwipesError.message}`,
-    );
-  }
-
-  const userIdToMessageMap: Map<string, string> = new Map(
-    likedSwipes.map((swipe) => [swipe.user_id, swipe.message]),
-  );
-
-  const likedUserIds = likedSwipes.map((swipe) => swipe.user_id);
-  // const likedProfiles = profileService.getProfiles(likedUserIds);
-
-  console.log(likedSwipes);
-  console.log(userIdToMessageMap);
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
