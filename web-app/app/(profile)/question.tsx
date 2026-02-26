ecaf829cb7648eb938b062e20ede7389ca265c78
Nate Levison <natelevison@gmail.com>
2026-02-25 19:25:40 -0500

    change component pattern for question

--- web-app/app/(profile)/question.tsx
+++ web-app/app/(profile)/question.tsx
@@ -5,46 +5,51 @@ import { Label } from "../../components/ui/label";
 import { cn } from "@/lib/utils"
 import { Textarea } from "../../components/ui/textarea";
 
-/**
- * The red star next to a required question.
- * @param required Whether or not to display the star.
- */
-function QuestionRequiredStar({ required }: { required: boolean }) {
-  if (required) {
-    return (
-      <span className={cn("text-red-500 ml-1")}>*</span>
-    )
-  }
-  return (<span></span>);
+type QuestionProps = {
+  question: string;
+  questionDataName: string;
 }
 
-/**
- * Component with an input (like a radio or checkbox) with a label below it.
- * The width of the label does not effect the width of the component.
- * @param textLabel The label below the option item.
- * @param dataName The name in the form of the response.
- * @param type The type of input (like "checkbox", "radio").
- */
-function QuestionOptionItem({ textLabel, dataName, type }: { textLabel: string, dataName: string, type: string }) {
-  const id = `${dataName}-radio-${textLabel}`;
-  return (
-    <div id={`${id}-container`} className={cn("relative flex flex-col items-center basis-4 w-sm")}>
-      <Input type={type} name={dataName} id={id} className={cn("h-fit")}/>
-      <Label className={cn("absolute top-full mt-1 whitespace-nowrap text-neutral-500 text-sm")} htmlFor={id}>{textLabel}</Label>
-    </div>
-  )
+type QuestionPropsScale = QuestionProps & {
+  scaleOptions: any[];
 }
+
+// /**
+//  * The red star next to a required question.
+//  * @param required Whether or not to display the star.
+//  */
+// function QuestionRequiredStar({ required }: { required: boolean }) {
+//   if (required) {
+//     return (
+//       <span className={cn("text-red-500 ml-1")}>*</span>
+//     )
+//   }
+//   return (<span></span>);
+// }
+
+// /**
+//  * Component with an input (like a radio or checkbox) with a label below it.
+//  * The width of the label does not effect the width of the component.
+//  * @param textLabel The label below the option item.
+//  * @param dataName The name in the form of the response.
+//  * @param type The type of input (like "checkbox", "radio").
+//  */
+// function QuestionOptionItem({ textLabel, dataName, type }: { textLabel: string, dataName: string, type: string }) {
+//   return (
+    
+//   )
+// }
 
 /**
  * Displays the options for a question and aligns them, along with the question text itself.
+ * Used to align radio and multiple choice questions.
  * @param children The question text component.
  * @param items The `QuestionOptionItem`s to select from.
  */
-function Question({ children, items, required }: React.ComponentProps<"div"> & { items: any[], required: boolean }) {
+function QuestionScaleContainer({ children, items }: React.ComponentProps<"div"> & { items: any[] }) {
   return (<div className={cn("ml-2 mb-9")}>
     <h3 className={cn("mb-1")}>
       {children}
-      <QuestionRequiredStar required={required} />
     </h3>
     <div className={cn("flex gap-10")}>
       {items}
@@ -55,73 +60,65 @@ function Question({ children, items, required }: React.ComponentProps<"div"> & {
 /**
  * A radio question (select one).
  */
-function QuestionScale({ className, children, questionDataName, scaleOptions, required=true, ...props }: React.ComponentProps<"div"> & { questionDataName: string, scaleOptions: string[], required?: boolean}) {
+function QuestionScale({ question, questionDataName, scaleOptions }: QuestionPropsScale) {
   const items = [];
 
   for (let i = 0; i < scaleOptions.length; i++) {
+    const id = `${questionDataName}-radio-${question}`;
     items.push(
-      <QuestionOptionItem
-        textLabel={scaleOptions[i]}
-        dataName={questionDataName}
-        type="radio"
-        key={`${questionDataName}-${i}`}
-      />
+      <div id={`${id}-container`} className={cn("relative flex flex-col items-center basis-4 w-sm")}>
+        <Input type="radio" name={questionDataName} id={id} className={cn("h-fit")}/>
+        <Label className={cn("absolute top-full mt-1 whitespace-nowrap text-neutral-500 text-sm")} htmlFor={id}>{scaleOptions[i]}</Label>
+      </div>
     );
   }
 
-  return (<Question items={items} required>{children}</Question>);
+  return (<QuestionScaleContainer items={items}>{question}</QuestionScaleContainer>);
 }
 
 /**
  * A checkbox question (select multiple).
  */
-function QuestionScaleMultiple({ className, children, questionDataName, scaleOptions, required=true, ...props }: React.ComponentProps<"div"> & { questionDataName: string, scaleOptions: string[], required?: boolean}) {
+function QuestionScaleMultiple({ question, questionDataName, scaleOptions }: QuestionPropsScale) {
   const items = [];
 
   for (let i = 0; i < scaleOptions.length; i++) {
+    const id = `${questionDataName}-radio-${question}`;
     items.push(
-      <QuestionOptionItem
-        textLabel={scaleOptions[i]}
-        dataName={questionDataName}
-        type="checkbox"
-        key={`${questionDataName}-${i}`}
-      />
-    );
+      <div id={`${id}-container`} className={cn("relative flex flex-col items-center basis-4 w-sm")}>
+        <Input type="checkbox" name={questionDataName} id={id} className={cn("h-fit")}/>
+        <Label className={cn("absolute top-full mt-1 whitespace-nowrap text-neutral-500 text-sm")} htmlFor={id}>{scaleOptions[i]}</Label>
+      </div>
+    )
   }
 
-  return (<Question items={items} required>{children}</Question>);
+  return (<QuestionScaleContainer items={items}>{question}</QuestionScaleContainer>);
 }
 
 /**
  * A freeform textarea question.
  */
-function QuestionFreeform({ className, children, questionDataName, required=true, ...props }: React.ComponentProps<"div"> & { questionDataName: string, required?: boolean}) {
-  return (<div className={cn("ml-2 mb-9")}>
-    <h3 className={cn("mb-1")}>
-      {children}
-      <QuestionRequiredStar required={required} />
-    </h3>
-    <div>
+function QuestionFreeform({ question, questionDataName }: QuestionProps) {
+  return (
+    <div className={cn("ml-2 mb-9")}>
+      <h3 className={cn("mb-1")}>{question}</h3>
       <Textarea name={questionDataName} placeholder=". . ." className={cn("w-xs")} />
     </div>
-  </div>);
+  );
 }
 
 /**
  * A yes or no question.
  * TODO make it a proper switch. Right now it looks awful.
  */
-function QuestionBoolean({ className, children, questionDataName, required=true, ...props }: React.ComponentProps<"div"> & { questionDataName: string, required?: boolean}) {
-  return <div className={cn("ml-2 mb-9")}>
-    <h3 className={cn("mb-1")}>
-      {children}
-      <QuestionRequiredStar required={required} />
-    </h3>
-    <i className={cn("text-xs text-neutral-500")}>placeholder for a proper switch component</i>
-    <div>
+function QuestionBoolean({ question, questionDataName }: QuestionProps) {
+  return (
+    <div className={cn("ml-2 mb-9")}>
+      <h3 className={cn("mb-1")}>{question}</h3>
+      <i className={cn("text-xs text-neutral-500")}>placeholder for a proper switch component</i>
       <Input type="checkbox" className={cn("h-9 w-6")} />
     </div>
-  </div>
+  );
 }
 
-export { QuestionScale, QuestionScaleMultiple, QuestionRequiredStar, QuestionFreeform, QuestionBoolean };
+export { QuestionScale, QuestionScaleMultiple, QuestionFreeform, QuestionBoolean };
--- web-app/app/(profile)/questions-test/page.tsx
+++ web-app/app/(profile)/questions-test/page.tsx
@@ -1,77 +1,78 @@
-import { QuestionScale, QuestionScaleMultiple, QuestionRequiredStar, QuestionFreeform, QuestionBoolean } from "@/app/(profile)/question";
+import { QuestionScale, QuestionScaleMultiple, QuestionFreeform, QuestionBoolean } from "@/app/(profile)/question";
 import { requireAuth } from "@/lib/auth";
+import { useEffect } from "react";
 
 /**
  * A testing page for the form components.
  */
 export default async function FormTestPage() {
   const user = await requireAuth();
+  
+  useEffect(() => {
+    // This runs only on the client
+    const inputs = document.querySelectorAll("input");
+    console.log(inputs); // NodeList of all input elements
+  }, []);
+
 
   return (
     <div className="min-h-screen bg-zinc-50 font-sans dark:bg-black p-3">
-      <div className="max-w-lg mb-4">
+      {/* <div className="max-w-lg mb-4">
         <i className="text-sm/3 text-neutral-400">
           <QuestionRequiredStar required /> = required question
         </i>
-      </div>
+      </div> */}
       <section>
         <h2 className="text-lg font-bold">Roommate Habits</h2>
         <ol className="list-decimal ml-4">
           <li>
             <QuestionScale
+              question="How often are you okay with guests during the day?"
               questionDataName="guests-day"
               scaleOptions={["Never", "", "", "", "Always"]}
-            >
-              How often are you okay with guests during the day?
-            </QuestionScale>
+            />
           </li>
           <li>
             <QuestionScale
+              question="How often are you okay with overnight guests?"
               questionDataName="guests-night"
               scaleOptions={["Never", "", "", "", "Always"]}
-            >
-              How often are you okay with guests spending the night?
-            </QuestionScale>
+            />
           </li>
           <li>
             <QuestionScale
+              question="How tidy do you want to keep your dorm/apt?"
               questionDataName="cleanliness"
               scaleOptions={["Dirty", "", "", "", "Pristine"]}
-            >
-              How tidy do you want to be?
-            </QuestionScale>
+            />
           </li>
           <li>
             <QuestionScale
+              question="How comfortable with sharing supplies (like paper towels, tp...) are you?"
               questionDataName="sharing"
               scaleOptions={["Hardly", "", "", "", "Very"]}
-            >
-              How comfortable with sharing things (like paper towels) are you?
-            </QuestionScale>
+            />
           </li>
           <li>
             <QuestionScale
+              question="How often do you need privacy?"
               questionDataName="privacy"
               scaleOptions={["Never", "", "", "", "Always"]}
-            >
-              How often do you need privacy?
-            </QuestionScale>
+            />
           </li>
           <li>
             <QuestionScale
+              question="How often do you need quiet (like studying or meetings)?"
               questionDataName="quiet"
               scaleOptions={["Never", "", "", "", "Always"]}
-            >
-              How often do you need quiet (like studying or meetings)?
-            </QuestionScale>
+            />
           </li>
           <li>
             <QuestionScale
+              question="When do you wake up?"
               questionDataName="wake"
               scaleOptions={["Earlier", "6AM", "7AM", "8AM", "9AM", "10AM", "Later"]}
-            >
-              When do you wake up?
-            </QuestionScale>
+            />
           </li>
         </ol>
       </section>
@@ -81,25 +82,22 @@ export default async function FormTestPage() {
         <ol className="list-decimal ml-4">
           <li>
             <QuestionScaleMultiple
+              question="What year(s) are you okay to room with?"
               questionDataName="years"
               scaleOptions={["1st", "2nd", "3rd", "4th", "Grad"]}
-            >
-              What year(s) are you okay to room with?
-            </QuestionScaleMultiple>
+            />
           </li>
           <li>
             <QuestionFreeform
+              question="What languages other than English do you speak?"
               questionDataName="language"
-            >
-              What languages other than English do you speak?
-            </QuestionFreeform>
+            />
           </li>
           <li>
             <QuestionBoolean
+              question="Are you okay with a smoker?"
               questionDataName="smoker"
-            >
-              Are you okay with a smoker?
-            </QuestionBoolean>
+            />
           </li>
         </ol>
       </section>
