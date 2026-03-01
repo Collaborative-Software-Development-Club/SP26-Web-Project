f43397e71b1c42c2d7b4f1ec73816c04a5e830e6
donutdiet <72674098+donutdiet@users.noreply.github.com>
2026-03-01 14:28:33 -0500

    Create save roommate preference action

--- web-app/app/(match)/discovery/_actions.ts
+++ web-app/app/(match)/discovery/_actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { createClient } from "@/lib/supabase/server";
-import { UserProfile, LikedYouProfile } from "./types";
+import { UserProfile, LikedYouProfile, RoommatePreference } from "./types";
 
 export async function getLikedYouProfiles() {
   const supabase = await createClient();
@@ -12,7 +12,7 @@ export async function getLikedYouProfiles() {
   if (!user) {
     throw new Error("Unauthorized");
   }
-    const { data: likedSwipes, error: likedSwipesError } = await supabase
+  const { data: likedSwipes, error: likedSwipesError } = await supabase
     .from("discovery_swipes")
     .select("user_id, created_at, message")
     .eq("target_user_id", user.id)
@@ -32,10 +32,10 @@ export async function getLikedYouProfiles() {
 
   // const likedProfiles = await profileService.getProfiles(likedSwipes.map((swipe) => swipe.user_id);
 
-//   const likedYouProfiles: LikedYouProfile[] = likedProfiles.map((profile) => ({
-//     ...profile,
-//     message: userIdToMessageMap.get(profile.user_id) ?? "",
-//   }));
+  //   const likedYouProfiles: LikedYouProfile[] = likedProfiles.map((profile) => ({
+  //     ...profile,
+  //     message: userIdToMessageMap.get(profile.user_id) ?? "",
+  //   }));
 
   return userIdToMessageMap;
   //return likedYouProfiles;
@@ -219,5 +219,36 @@ export async function getUserRoommatePreferences(user_id: string) {
       importance: item.importance,
     }));
     return preferenceIds;
+  }
+}
+
+export async function saveUserRoommatePreferences(
+  preferences: RoommatePreference[],
+) {
+  const supabase = await createClient();
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
+
+  if (userError || user === null) {
+    throw new Error(`Error fetching current user: ${userError?.message}`);
+  }
+
+  const { error: saveError } = await supabase
+    .from("discovery_roommate_preferences")
+    .upsert(
+      preferences.map((preference) => ({
+        user_id: user.id,
+        preference_id: preference.preference_id,
+        importance: preference.importance,
+      })),
+      { onConflict: "user_id,preference_id" },
+    );
+
+  if (saveError) {
+    throw new Error(
+      `Failed to save user roommate preferences: ${saveError.message}`,
+    );
   }
 }
