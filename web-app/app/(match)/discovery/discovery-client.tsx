d46c2632b42945225d4ec0a3ae5456f01d61f4b8
Allen <162939876+recur312@users.noreply.github.com>
2026-03-24 15:55:06 -0400

    add no-more-results view for discovery page

--- web-app/app/(match)/discovery/discovery-client.tsx
+++ web-app/app/(match)/discovery/discovery-client.tsx
@@ -18,6 +18,8 @@ export function DiscoveryClient({
     profiles[0],
   );
 
+  const [reachedEnd, setReachedEnd] = useState(false);
+
   const handleNext = () => {
     const currentIndex = profiles.findIndex(
       (p) => p.user_id === selectedProfile.user_id,
@@ -25,7 +27,8 @@ export function DiscoveryClient({
     if (currentIndex < profiles.length - 1) {
       setSelectedProfile(profiles[currentIndex + 1]);
     } else {
-      setSelectedProfile(profiles[0]);
+      //setSelectedProfile(profiles[0]);
+      setReachedEnd(true);
     }
   };
 
@@ -33,21 +36,59 @@ export function DiscoveryClient({
     const currentIndex = profiles.findIndex(
       (p) => p.user_id === selectedProfile.user_id,
     );
-    if (currentIndex > 0) {
-      setSelectedProfile(profiles[currentIndex - 1]);
-    } else {
-      setSelectedProfile(profiles[profiles.length - 1]);
+    if(!reachedEnd){
+      if (currentIndex > 0) {
+        setSelectedProfile(profiles[currentIndex - 1]);
+      } 
+      else {
+        setSelectedProfile(profiles[profiles.length - 1]);
+      }
+    }else{
+      setReachedEnd(false);
     }
   };
 
   return (
     <div className="w-full flex flex-col items-center justify-center mb-10">
       <Filter preferences={roommatePreferences}></Filter>
-      <ProfileCard
+      {reachedEnd 
+        ?<NoMoreResults handleBefore={handleBefore} profile={selectedProfile}/>
+        :<ProfileCard
         profile={selectedProfile}
         handleNext={handleNext}
         handleBefore={handleBefore}
-      />
+        />
+      }
+    </div>
+  );
+}
+
+function NoMoreResults({
+  handleBefore,
+  profile,
+}: {
+  handleBefore: () => void;
+  profile: DiscoveryProfile;
+}) {
+  return (
+    <div className="w-full bg-zinc-50 dark:bg-black p-4 md:p-8 font-sans flex flex-col items-center">
+      <div className="w-3/4 max-w-4xl  overflow-hidden relative md:h-[560px] flex flex-col center-items text-center">
+        <span className="text-5xl">✦</span>
+        <h2 className="px-16 pt-16 text-xl font-semibold text-zinc-800 dark:text-zinc-200">
+          You're all caught up
+        </h2>
+        <p className="px-12 pt-8 text-zinc-400 dark:text-zinc-100">
+          You've seen everyone. Try adjusting your filters to discover more
+          people
+        </p>
+        <div className="p-[1rem] mt-auto flex flex-col items-center">
+          <UndoButton
+            handleBefore={handleBefore}
+            isDiscovery={true}
+            targetUserId={profile.user_id}
+          />
+        </div>
+      </div>
     </div>
   );
 }
