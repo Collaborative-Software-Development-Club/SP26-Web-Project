5deeb63b24282bd2275c43bf924bf76ff255e952
Nate Levison <natelevison@gmail.com>
2026-04-15 13:36:06 -0400

    finish ui

--- web-app/app/(profile)/profile/_components/living-habits-page.tsx
+++ web-app/app/(profile)/profile/_components/living-habits-page.tsx
@@ -1,24 +1,26 @@
 "use client";
-import majors from "@/mock/majors.json";
-import hobbies from "@/mock/hobbies.json";
 import { useState } from "react";
 import { Card, CardHeader, CardContent, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { Select, SelectContent, SelectTrigger, SelectValue, SelectItem } from "@/components/ui/select";
-import { Textarea } from "@/components/ui/textarea";
 
 import { preferenceQuestion } from "@/lib/constants/preference-question";
+import { valueForOptionIndex } from "../create-profile/helpers";
 
-import type { UserProfile } from "@/app/(profile)/types";
+import type { Preference, UserProfile } from "@/app/(profile)/types";
 type ProfilePageProps = {
   profile: UserProfile
 }
-export default function LivingHabitsPage({ profile }: ProfilePageProps) {
-  const [darkMode, setDarkMode] = useState(false);
-  const [formData, setFormData] = useState<UserProfile>(profile);
 
+function prefsEqual(p1: Preference[], p2: Preference[]) {
+  return JSON.stringify(p1) === JSON.stringify(p2);
+}
+
+export default function LivingHabitsPage({ profile }: ProfilePageProps) {
+  const [formData, setFormData] = useState<UserProfile>(profile);
+  const [formModified, setFormModified] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const oldPrefs = profile.preferences;
 
   return (
     <div className="h-full w-full overflow-auto bg-zinc-50 p-8 dark:bg-black">
@@ -51,23 +53,65 @@ export default function LivingHabitsPage({ profile }: ProfilePageProps) {
           </div>
 
           <div className="space-y-6">
-            {profile.preferences.map(pref => {
-              function onChange() {}
+            {formData.preferences.map(pref => {
               
               const entry = preferenceQuestion.get(pref.name);
               if (entry === undefined) {
                 return (<div key={`pref-${pref.preference_id}`}>Something went really wrong</div>);
               }
               
-              return (<>
-                <div key={`pref-${pref.preference_id}`} className="space-y-2">
-                  <Label htmlFor={pref.preference_id}>{entry.question}</Label>
-                  <Input id={pref.preference_id} defaultValue={pref.value} onChange={onChange}></Input>
+              return (<div key={`pref-${pref.preference_id}`}>
+                <Label className="mb-2" htmlFor={pref.preference_id}>{entry.question}</Label>
+                <div className="space-y-2 flex flex-col w-75">
+
+                  {entry.options.map((opt, i) => {
+                    const v = valueForOptionIndex(entry.options, i);
+                    const selected = pref.value === v;
+                    return (
+                      <Button
+                        key={`${pref.preference_id}-${opt}`}
+                        type="button"
+                        variant={selected ? "default" : "outline"}
+                        className="capitalize"
+                        onClick={() => {
+                          // Mutate the form data.
+                          setFormData({
+                            ...formData, // put all the old shit back in
+                            preferences: formData.preferences.map(p => { // modify preferences
+                              if (p.preference_id === pref.preference_id) {
+                                return { ...p, value: v};
+                              } else {
+                                return p;
+                              }
+                            })
+                          });
+                          // setFormModified(oldPrefs == formData.preferences);
+                          console.log(opt, i, pref);
+                          // console.log(oldPrefs);
+                        }}
+                        disabled={isSubmitting}
+                      >
+                        {opt}
+                      </Button>
+                    );
+                  })}
+                  
                 </div>
-              </>);
+              </div>);
             })}
-            <div className="flex justify-end">
-              <Button type="submit" className="px-6">
+            <div className="flex justify-end gap-2">
+              <Button className="px-6" disabled={prefsEqual(formData.preferences, oldPrefs)} onClick={() => {
+                console.log(formData.preferences.map(p=>p.value));
+                console.log(oldPrefs.map(p=>p.value));
+                setFormData({
+                  ...formData,
+                  preferences: oldPrefs.map(p => ({...p})), // bullshit so it mutates and react sees it
+                });
+                window.scrollTo({ top: -1 });
+              }}>
+                Undo All Changes
+              </Button>
+              <Button type="submit" className="px-6" disabled={prefsEqual(formData.preferences, oldPrefs)}>
                 Save Changes
               </Button>
             </div>
